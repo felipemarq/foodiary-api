1ce8395c7231decad12ab692e2a3857650767fb1
Felipe Marques <felipeshay2000@gmail.com>
2025-06-03 15:09:56 -0300

    feat: :sparkles: Zod Validation created

--- src/application/controllers/HelloController.ts
+++ src/application/controllers/HelloController.ts
@@ -1,14 +1,24 @@
-import { IController } from "../contracts/Controller";
+import { z } from "zod";
+import { Controller } from "../contracts/Controller";
 
-export class HelloController implements IController<unknown> {
+const schema = z.object({
+  name: z
+    .string({
+      message: "Name should be a string",
+    })
+    .min(2, "Name is too short"),
+  email: z.string().min(5, "Email is too short").email("Invalid email"),
+});
+
+export class HelloController extends Controller<unknown> {
+  protected override schema = schema;
   async handle(
-    request: IController.Request
-  ): Promise<IController.Response<unknown>> {
+    request: Controller.Request
+  ): Promise<Controller.Response<unknown>> {
     return {
       statusCode: 200,
       body: {
-        deuBom: true,
-        request,
+        parsedBody: request.body,
       },
     };
   }
--- src/main/adapters/lambdaHttpAdapter.ts
+++ src/main/adapters/lambdaHttpAdapter.ts
@@ -1,24 +1,55 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 import { lambdaBodyParser } from "../utils/lambdaBodyParser";
-import { IController } from "../../application/contracts/Controller";
+import { Controller } from "../../application/contracts/Controller";
+import { ZodError } from "zod";
 
-export function lambdaHttpAdapter(controller: IController<unknown>) {
+export function lambdaHttpAdapter(controller: Controller<unknown>) {
   return async (
     event: APIGatewayProxyEventV2
   ): Promise<APIGatewayProxyResultV2> => {
-    const body = lambdaBodyParser(event.body);
-    const params = event.pathParameters ?? {};
-    const queryParams = event.queryStringParameters ?? {};
+    try {
+      const body = lambdaBodyParser(event.body);
+      const params = event.pathParameters ?? {};
+      const queryParams = event.queryStringParameters ?? {};
 
-    const response = await controller.handle({
-      body,
-      params,
-      queryParams,
-    });
+      const response = await controller.exectute({
+        body,
+        params,
+        queryParams,
+      });
 
-    return {
-      statusCode: response.statusCode,
-      body: response.body ? JSON.stringify(response.body) : undefined,
-    };
+      console.log("Event: ", event);
+
+      return {
+        statusCode: response.statusCode,
+        body: response.body ? JSON.stringify(response.body) : undefined,
+      };
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            error: {
+              code: "VALIDATION",
+              message: error.issues.map((issue) => ({
+                field: issue.path.join("."),
+                message: issue.message,
+              })),
+            },
+          }),
+        };
+      }
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: {
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Internal server error",
+          },
+        }),
+      };
+    }
   };
 }
