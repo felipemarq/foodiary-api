45c17632c059ee93763683ee41602586c4ea8377
Felipe Marques <felipeshay2000@gmail.com>
2025-07-10 21:43:22 -0300

    feat: :sparkles: Autenticação completa

---
+++ src/main/functions/auth/cognito/customMessageTrigger.ts
@@ -0,0 +1,23 @@
+import ForgotPassword from "@infra/emails/templates/auth/forgotPassword";
+import { render } from "@react-email/render";
+import { CustomMessageTriggerEvent } from "aws-lambda";
+
+export async function handler(event: CustomMessageTriggerEvent) {
+  console.log("Evento recebido:", JSON.stringify(event, null, 2));
+
+  if (event.triggerSource === "CustomMessage_ForgotPassword") {
+    const confirmationCode = event.request.codeParameter;
+    const email = event.request.userAttributes?.email;
+
+    console.log("Código de confirmação:", confirmationCode);
+    console.log("E-mail de destino:", email);
+
+    const html = await render(ForgotPassword({ confirmationCode }));
+    console.log("HTML gerado para o e-mail:", html);
+
+    event.response.emailSubject = "🍏 foodiary | Recupere a sua conta!";
+    event.response.emailMessage = html;
+  }
+
+  return event;
+}
